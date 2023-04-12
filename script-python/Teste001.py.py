942836d011df3b8f98ffc97a54fc5f37fa3366b7
misa_misa <105995479+mandrejezieski@users.noreply.github.com>
2023-04-12 00:42:54 -0300

    001

---
+++ script-python/Teste001.py.py
@@ -0,0 +1,5 @@
+nome = input( "Digite seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+print("----------------------------------------------------")
+print ("Seu nome é ",nome,"\nSua idade é: ",idade, "\nSeu peso é: ",peso)
